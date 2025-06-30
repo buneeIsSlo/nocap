b2cc122c9f2a0421e09e2ee523a29c70393ab948
abhi <buneeisslo@gmail.com>
2025-06-30 16:33:19 +0530

    Refactor: next config change domain to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com", // widely used for google avatars
-      "lh4.googleusercontent.com",
-      "lh5.googleusercontent.com",
-      "lh6.googleusercontent.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com", // widely used for google avatars
+      },
     ],
   },
   /* config options here */
